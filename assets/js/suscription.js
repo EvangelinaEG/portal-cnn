2290eed76823e639df930c528556a3fb1d0c05a9
evangelina <evangelina.gomes@ellecktra.com>
2020-11-01 00:59:01 -0300

    Validation of the input name

--- assets/js/suscription.js
+++ assets/js/suscription.js
@@ -1,15 +1,44 @@
 
 window.addEventListener('load', function(){
     
-    const inputElement = document.getElementById('name');
+    const name = document.getElementById('name');
+    const  email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const repassword = document.getElementById('repassword');
+    const age = document.getElementById('age');
+    const phone = document.getElementById('phone');
+    const address = document.getElementById('address');
+    const city = document.getElementById('city');
+    const zip = document.getElementById('zip');
+    const dni = document.getElementById('dni');
     const sus = document.getElementById('subscriber');
-    
 
-    inputElement.addEventListener('keypress', e =>
+    name.addEventListener('keypress', e =>
     {
         setTimeout(() => sus.textContent = e.target.value, 0);
     });
 
-    
+    name.addEventListener("blur", function( event ) {
 
+        const name = event.target.value;
+        const pattern = new RegExp('^[a-zA-Z\\s]+$');
+    
+        if(name.length < 6 || !pattern.test(name) || (name.indexOf(" ") == -1)){
+            
+            document.getElementById('name').nextElementSibling.classList.remove('hidden');
+            document.getElementById('name').nextElementSibling.classList.add('danger');
+            return false;
+        }    
+    }, true);
+
+    name.addEventListener("focus", function( event ) {
+        
+            document.getElementById('name').nextElementSibling.classList.remove('danger');
+            document.getElementById('name').nextElementSibling.classList.add('hidden');
+            return false;
+            
+    }, true);
+
+  
+    
 });
